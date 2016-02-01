7b5c58a936b1ab01fb04eeaace1ab2eb7a6a02b1
Robert Hohman <rhohman@svdev.odonnellpdc.com>
2016-02-01 14:44:50 -0500

    added the send option

--- multimail.py
+++ multimail.py
@@ -5,21 +5,28 @@ import os
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEImage import MIMEImage
-
+import smtplib
 
 # Argparse setup
 parser = argparse.ArgumentParser(description='Builds an multi-part html email.')
 parser.add_argument('html_file', action="store", help='A HMTL file that will become the HTML portion the email')
 parser.add_argument('text_file', action="store", help='A text file that will become the text portion of the email')
-parser.add_argument('-i' , '--images', action="append", help='adds images to the email')
+parser.add_argument('-i' , '--images',nargs="*", help='A list images to add to the email')
+parser.add_argument('-s' , '--send',  nargs=3,   help='Sends the email form the local host and needs a sender, recipient, and a subject.', metavar=('FROM','TO','SUBJECT'))
 args = parser.parse_args()
+
 
 HTML_FILE = args.html_file
 TEXT_FILE = args.text_file
 IMAGES    = args.images
+SEND      = args.send
 
 def main():
     msgRoot = MIMEMultipart('alternative')
+    if SEND:
+        msgRoot['To'] = SEND[0]
+        msgRoot['From'] = SEND[1]
+        msgRoot['Subject'] = SEND[2]
     msgRoot.preamble = 'This is a multi-part message in MIME format.'
     with open(TEXT_FILE, 'r') as txt_f:
         text = txt_f.read()
@@ -37,7 +44,11 @@ def main():
                 msgImage.add_header('Content-ID', os.path.split(image)[1]) ## clean up to remove the folder location in the for cid
                 msgRelated.attach(msgImage)        
         msgRoot.attach(msgRelated)
-    print(msgRoot.as_string()) 
+        if SEND:
+            smtp = smtplib.SMTP('localhost')
+            smtp.sendmail(SEND[0], SEND[1], msgRoot.as_string())
+            smtp.quit()
+        print(msgRoot.as_string()) 
 
 
 if __name__ == "__main__":
